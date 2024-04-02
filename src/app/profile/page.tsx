41c25b4c1d7538116862b7f33617cc618a29a375
HOANG THANG <thaithang115@gmail.com>
2024-04-02 16:21:13 +0700

    get familiar with Material-UI

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Button from "@mui/material/Button";
 import { useRouter } from "next/navigation";
 import LayoutFooter from "../Layout/index";
 import "../style/views/profile.css";
@@ -9,15 +10,24 @@ function Profile() {
   return (
     <LayoutFooter>
       <div className="profile_container">
-        <button
-          className="btn_logout"
+        <h1>Welcome to Next.js with Material-UI</h1>
+        <Button
+          variant="contained"
+          color="error"
           onClick={() => {
             localStorage.removeItem("userInfo");
             router.push("/");
           }}
         >
           Log out
-        </button>
+        </Button>
+        {/* <button
+          className="btn_logout"
+          onClick={() => {
+            localStorage.removeItem("userInfo");
+            router.push("/");
+          }}
+        ></button> */}
       </div>
     </LayoutFooter>
   );
