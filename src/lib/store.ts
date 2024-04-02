c6845266e33d4569ddddb3ef7199ec381e9f1e9d
HOANG THANG <thaithang115@gmail.com>
2024-04-02 15:23:55 +0700

    setup ci cd

---
+++ src/lib/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+4;
+import todoSlice from "./features/todos/todoSlice";
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      // user: userSlice,
+      todoList: todoSlice,
+    },
+  });
+};
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
